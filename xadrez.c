d42bd500897b78077abb822c214ed108a56edb36
Wellerson96 <w.cst96@gmail.com>
2025-02-20 12:00:41 +0000

    Movimentação cavalo

--- xadrez.c
+++ xadrez.c
@@ -27,17 +27,51 @@ int main() {
     }while(torre <= 5);
 
     // Utilização do "for" para mover a rainha.
-    int rainha = 1;
+    int rainha;
 
     printf("Rainha se movimenta para:\n");
 
-    for(rainha; rainha <= 5; rainha ++){
+    for(rainha = 1; rainha <= 5; rainha ++){
         printf("esquerda.\n");
     }
 
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
     // Um loop pode representar a movimentação horizontal e outro vertical.
+
+    // Utilização do loop aninhado "for" para mover o cavalo.
+    int cavalo;
+    int cavalo2;
+
+    printf("Cavalo se movimenta para:\n");
+
+    for(cavalo = 1; cavalo <= 1; cavalo++){
+        for(cavalo2 = 1; cavalo2 <=2; cavalo2++){
+            printf("baixo.\n");
+        }
+
+printf("esquerda.\n");
+
+    }
+
+    // Utilização do loop aninhado "while" para mover o cavalo.
+    int cavalo3 = 1;
+    int cavalo4 = 1;
+
+    printf("Cavalo se movimenta para:\n");
+
+    while(cavalo3 <= 1){
+        cavalo3 ++;
+        while(cavalo4 <= 2){
+
+            printf("baixo.\n");
+
+            cavalo4 ++;
+        }
+
+        printf("esquerda.\n");
+
+    }
 
     // Nível Mestre - Funções Recursivas e Loops Aninhados
     // Sugestão: Substitua as movimentações das peças por funções recursivas.
